b9ac03cb9d05709f632025babe7e2a1b902b55fd
Lais Kagawa <lalakagawa@hotmail.com>
2019-12-01 22:02:27 -0300

    feat: add Watson natural language understanding service

--- credentials/README.md
+++ credentials/README.md
@@ -9,3 +9,18 @@ File `algorithimia.json`
     "apiKey": "simjSqoygeHm1LphHz8uGrKJ4X61"
 }
 ```
+
+## Watson Natural Language Understanding
+
+File `watson-nlu.json`
+
+```
+{
+  "apikey": "AHF7z-S5L0da5aWuJSdonJLtvizmiXZxCyC_iYgVlZwU",
+  "iam_apikey_description": "Auto-generated for key 67890cff-c146-49fe-8ea8-2ff8e55c376e",
+  "iam_apikey_name": "Auto-generated service credentials",
+  "iam_role_crn": "crn:v1:bluemix:public:iam::::serviceRole:Manager",
+  "iam_serviceid_crn": "crn:v1:bluemix:public:iam-identity::a/59085211132d48dab6789432c7c19bc2::serviceid:ServiceId-fff3fbb3-0eb4-4ae7-b5f3-c6f1f8a47ab3",
+  "url": "https://gateway.watsonplatform.net/natural-language-understanding/api"
+}
+```
--- robots/text.js
+++ robots/text.js
@@ -1,60 +1,95 @@
-const algorithmia = require('algorithmia')
-const algorithmiaApiKey = require('./../credentials/algorithmia.json').apiKey
-const sentenceBoundaryDetection = require('sbd')
+const algorithmia = require("algorithmia");
+const algorithmiaApiKey = require("./../credentials/algorithmia.json").apiKey;
+const sentenceBoundaryDetection = require("sbd");
+const watsonApiKey = require("../credentials/watson-nlu.json").apikey;
+const NaturalLanguageUnderstandingV1 = require("ibm-watson/natural-language-understanding/v1");
+const { IamAuthenticator } = require("ibm-watson/auth");
+const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+  version: "2019-07-12",
+  authenticator: new IamAuthenticator({
+    apikey: watsonApiKey
+  }),
+  url: "https://gateway.watsonplatform.net/natural-language-understanding/api/"
+});
 
-async function robot (content) {
-    await fetchContentWikipedia(content)
-    sanitizeContent(content)
-    breakIntoSentences(content)
 
-    console.log(fetchContentWikipedia(content))
 
-    async function fetchContentWikipedia(content) {
-        const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
-        const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2?timeout=300')
-        const wikipediaReponse = await wikipediaAlgorithm.pipe(content.searchTerm)
-        const wikipediaContent = wikipediaReponse.get()
+async function robot(content) {
+  await fetchContentWikipedia(content)
+  sanitizeContent(content)
+  breakIntoSentences(content)
+  limitMaximumSentences(content)
+  await fetchKeywordsOfAllSentences(content)
+  
+  console.log(content)
+  
+  async function fetchContentWikipedia(content) {
+      const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
+      const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2?timeout=300')
+      const wikipediaReponse = await wikipediaAlgorithm.pipe(content.searchTerm)
+      const wikipediaContent = wikipediaReponse.get()
+      content.sourceContentOriginal = wikipediaContent.content
+  }
 
-        content.sourceContentOriginal = wikipediaContent.content
-    }
+  function sanitizeContent(content) {
+      const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkDown(content.sourceContentOriginal)
+      const withoutDatesInParentheses = removeDatesInParentheses(withoutBlankLinesAndMarkdown)
+      content.sourceContentSanitized = withoutDatesInParentheses
+      function removeBlankLinesAndMarkDown(text) {
+          const allLines = text.split('\n')
+          const withoutBlankLinesAndMarkdown = allLines.filter((line) => {
+              if(line.trim().length ===0 || line.trim().startsWith('=')){
+                  return false
+              }
+              return true
+          })
+          return withoutBlankLinesAndMarkdown.join(' ')
+      }
+      function removeDatesInParentheses(text) {
+          return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g,' ')
+      }
+  }
 
-    function sanitizeContent(content) {
-        const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkDown(content.sourceContentOriginal)
-        const withoutDatesInParentheses = removeDatesInParentheses(withoutBlankLinesAndMarkdown)
+  function breakIntoSentences(content) {
+      content.sentences = []
+      const sentences = sentenceBoundaryDetection.sentences(content.sourceContentSanitized)
+      sentences.forEach((sentence) => {
+          content.sentences.push({
+              text: sentence,
+              keyword: [],
+              images: []
+          })
+      })
+  }
 
-        content.sourceContentSanitized = withoutDatesInParentheses
+  function limitMaximumSentences(content) {
+      content.sentences = content.sentences.slice(0, content.maximumSentences)
+  }
 
-        function removeBlankLinesAndMarkDown(text) {
-            const allLines = text.split('\n')
-            const withoutBlankLinesAndMarkdown = allLines.filter((line) => {
-                if(line.trim().length ===0 || line.trim().startsWith('=')){
-                    return false
-                }
-                return true
-            })
+  async function fetchKeywordsOfAllSentences(content) {
+      for(const sentence of content.sentences) {
+          sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text)
+      }
+  }
 
-            return withoutBlankLinesAndMarkdown.join(' ')
+  async function fetchWatsonAndReturnKeywords(sentence) {
+    const response = await naturalLanguageUnderstanding
+      .analyze({
+        text: sentence,
+        features: {
+          keywords: {}
         }
+      })
+      if(response.err){
+          throw response.err
+      }
 
-        function removeDatesInParentheses(text) {
-            return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g,' ')
-        }
-    }
-
-    function breakIntoSentences(content) {
-        content.sentences = []
-        const sentences = sentenceBoundaryDetection.sentences(content.sourceContentSanitized)
-
-        sentences.forEach((sentence) => {
-            content.sentences.push({
-                text: sentence,
-                keyword: [],
-                images: []
-            })
-        })
-
-        console.log(content.sentences)
-    }
+      const keywords = response.result.keywords.map(keyword => {
+                  return keyword.text
+                })
+      return keywords
+  }
 }
 
-module.exports = robot
+
+module.exports = robot;
