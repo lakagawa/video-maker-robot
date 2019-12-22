2b20a411ad2a334e4d74e73380645c2deadc1fb1
Lais Kagawa <lalakagawa@hotmail.com>
2019-12-21 22:27:13 -0300

    feat: add google images search

---
+++ robots/image.js
@@ -0,0 +1,45 @@
+const google = require('googleapis').google
+const customSearch = google.customsearch('v1')
+const state = require('./state.js')
+
+const googleSearchCredentials = require('../credentials/google-search.json')
+
+async function robot() {
+    const content = state.load()
+
+    await fetchAllImagesOfAllSentences(content)
+
+    state.save(content)
+
+    console.dir(content, { depth: null})
+
+
+    async function fetchAllImagesOfAllSentences(content) {
+        for(const sentence of content.sentences){
+            const query = `${content.searchTerm} ${sentence.keywords[0]}`
+            sentence.images = await fetchGoogleAndReturnImagesLinks(query)
+
+            sentence.googleSearchQuery = query
+        }
+    }
+
+    async function fetchGoogleAndReturnImagesLinks(query) {
+        const response = await customSearch.cse.list({
+            auth: googleSearchCredentials.apiKey,
+            cx: googleSearchCredentials.searchEngineId,
+            q: query,
+            searchType: 'image',
+            num: 2
+        }).catch(err => {
+            console.log(err)
+        })
+
+        const imagesUrl = response.data.items.map((item) => {
+            return item.link
+        })
+
+        return imagesUrl
+    }
+}
+
+module.exports = robot
